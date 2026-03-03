2852f22222886b0926bf326a927f36faad920038
Isonoda <isao.sonoda@gmail.com>
2026-03-03 14:58:13 +0900

    b1 cloudflareインストール

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
+import cloudflare from "@astrojs/cloudflare";
+
 export default defineConfig({
   site: "https://astro-simple-blog-omega.vercel.app/",
   integrations: [tailwind()],
+  adapter: cloudflare(),
 });
